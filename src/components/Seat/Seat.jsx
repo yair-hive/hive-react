b9eee62d6e86eec92fa1bac5da5f0e3aa96fe0dc
yair man <yairman2429@gmail.com>
2022-11-02 10:01:47 +0200

    adding the group color

--- src/components/Seat/Seat.jsx
+++ src/components/Seat/Seat.jsx
@@ -3,13 +3,20 @@ import "./Seat.css"
 
 class Seat extends React.Component{
     render(){
+        const GROUPS_COLOR = {
+            'א': 'rgb(25, 180, 64)',
+            'ב': 'rgb(180, 25, 115)'
+        }
+        const NAME_BOX_STYLE = {
+            backgroundColor: GROUPS_COLOR[this.props.group]
+        }
         return (
             <div>
                 <div className="seat">
                     <div className="num_box">
                         {this.props.number}    
                     </div> 
-                    <div className="name_box">
+                    <div className="name_box" style={NAME_BOX_STYLE}>
                         {this.props.name}
                     </div>          
                 </div>
